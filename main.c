eb78f6b01869e488f21efffa6e248846b1ef00ba
caalvarado6 <70360613+caalvarado6@users.noreply.github.com>
2020-10-01 22:33:41 -0600

    Update main.c

--- main.c
+++ main.c
@@ -16,9 +16,22 @@ int main(void) {
   scanf("%d %d", &s, &d);
 
   // TODO: Complete the rest of the categories
-  if(s < 120) {
-    if(d < 80) {
-      printf("Normal\n");
-    }
+  if(((s < 120)&&((d < 80)))&&((s > d ))){
+    printf("Normal\n");  //Blood pressure remains normal for values of s and d below 120 and 80 but s always has to be greater than d 
+  }
+  else if (((s >= 120)&&(s<=129))&&(d < 80)){
+    printf("Elevated\n");
+  }
+  else if (((s >= 130)&&(s<=139))||((d >= 80)&&(d <= 89))){
+    printf("Stage 1 Hypertension\n"); // AND and OR exprecions needed to indicate values range of values.
+  }
+  else if (((s > 140)&&(s < 181))||((d > 90)&&(d < 120))&&(s > d )){
+    printf("Stage 2 Hypertension\n");
+  }
+  else if (((s > 180)&&(d>120))&&((s > d ))){
+    printf("Hypertensive Crysis\n");
+  }
+  else if (s < d){
+    printf("Error\n");
   }
 }
